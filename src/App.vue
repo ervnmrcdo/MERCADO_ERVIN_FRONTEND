86907aa8f89dbc387c0ba8e7dfd1e84b3086924f
ervnmrcdo <ervinmercado31@gmail.com>
2026-06-17 08:21:15 +0800

    add(temp): router files

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TaskCounter from './components/TaskCounter.vue';
+import HelloWorld from "./components/HelloWorld.vue";
+import TaskCounter from "./components/TaskCounter.vue";
 </script>
 
 <template>
-  <TaskCounter />
+  <h1>Hello App!</h1>
+  <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
+  <nav>
+    <RouterLink to="/task-counter"> Go to Task Counter</RouterLink>
+    <RouterLink to="/task-list"> Go to Task List</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
+  <!-- <TaskCounter /> -->
 </template>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import TaskListView from './components/TaskListView.vue'
+import { router } from '../router/index.js'
 
-createApp(TaskListView).mount('#app')
+createApp(App).use(router).mount('#app')
