62dc1eabaa7d81b22a9f42f180b95aa8799b63f1
ervnmrcdo <ervinmercado31@gmail.com>
2026-06-19 19:25:16 +0800

    add: routerlinks

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,30 @@ import NavBar from "./components/NavBar.vue";
 
 <template>
   <main>
-    <NavBar class="navbar"/>
+    <!-- <NavBar class="navbar"/> -->
+    <div class="list-container">
+      <TransitionGroup>
+        <RouterLink class="link" to="/home">Home</RouterLink>
+        <RouterLink class="link" to="/task-counter">Task Counter</RouterLink>
+        <RouterLink class="link" to="/task-list">Task List</RouterLink>
+        <RouterLink class="link" to="/task-list-specific-view">Specific</RouterLink>
+        <RouterLink class="link" to="/todo-list-view">Todo List</RouterLink>
+        <RouterLink class="link" to="/user-list-view">User List</RouterLink>
+      </TransitionGroup>
+    </div>
     <RouterView />
-  </main> 
+  </main>
 </template>
+
+<style>
+.list-container {
+  gap: 5px;
+  border-style: solid;
+
+}
+
+.link {
+  color: white;
+  text-decoration: none;
+}
+</style>
