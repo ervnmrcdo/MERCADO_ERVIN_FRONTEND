4714cf87e9a0bfe185100b68eed428a8504e1c0e
ervnmrcdo <ervinmercado31@gmail.com>
2026-06-21 23:33:01 +0800

    add: back in stats page

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -5,17 +5,36 @@ import { useTaskStore } from "../stores/taskStore.js"
 import { storeToRefs } from "pinia";
 
 const taskStore = useTaskStore();
-const { doneCount, pendingCounts, totalCount } = storeToRefs(taskStore)
+const { doneCount, pendingCount, totalCount } = storeToRefs(taskStore)
 
 
 </script>
 
 <template>
+  <RouterLink to="task-list-specific-view">Back</RouterLink>
   <div class="stats-container">
-    stats page
+    Task Statistics
+    <ul>
+      <li>Total: {{ totalCount }}</li>
+      <li>Pending: {{pendingCount }}</li>
+      <li>Done: {{ doneCount }}</li>
+    </ul>
+
   </div>
 </template>
 
 
 
-<style></style>
+<style>
+
+
+.stats-container{
+  border-style: solid;
+  width:fit-content;
+  height:auto;
+  margin:auto;
+  margin-top: 10px;
+  padding: 40px;
+
+}
+</style>
