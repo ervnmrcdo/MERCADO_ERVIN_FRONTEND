e77df085e015d7dacbcc71f908aed3f60db80859
ervnmrcdo <ervinmercado31@gmail.com>
2026-06-21 23:11:13 +0800

    add: active on router link

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,53 @@
 <script setup>
 import NavBar from "./components/NavBar.vue";
-import {computed} from "vue"
-import {useRoute} from "vue-router"
-
+import { computed } from "vue";
+import { useRoute } from "vue-router";
 
 const route = useRoute();
-const currentRoute = computed(() => route.path)
-
-console.log(currentRoute)
-
+const currentRoute = computed(() => route.fullPath);
 </script>
 
 <template>
   <main>
     <!-- <NavBar class="navbar"/> -->
     <div class="list-container">
-    <TransitionGroup  class="links-wrapper" name="list" tag="RouterLink">
-        <RouterLink :class="{'active': true}" class="link" to="/home">Home</RouterLink>
-        <RouterLink class="link" to="/task-counter">Task Counter</RouterLink>
-        <RouterLink class="link" to="/task-list">Task List</RouterLink>
-        <RouterLink class="link" to="/task-list-specific-view">Specific</RouterLink>
-        <RouterLink class="link" to="/todo-list-view">Todo List</RouterLink>
-        <RouterLink class="link" to="/user-list-view">User List</RouterLink>
+      <TransitionGroup class="links-wrapper" name="list" tag="RouterLink">
+        <RouterLink
+          :class="{ active: currentRoute === '/home' }"
+          class="link"
+          to="/home"
+          >Home</RouterLink
+        >
+        <RouterLink
+          :class="{ active: currentRoute === '/task-counter' }"
+          class="link"
+          to="/task-counter"
+          >Task Counter</RouterLink
+        >
+        <RouterLink
+          :class="{ active: currentRoute === '/task-list' }"
+          class="link"
+          to="/task-list"
+          >Task List</RouterLink
+        >
+        <RouterLink
+          :class="{ active: currentRoute === '/task-list-specific-view' }"
+          class="link"
+          to="/task-list-specific-view"
+          >Specific</RouterLink
+        >
+        <RouterLink
+          :class="{ active: currentRoute === '/todo-list-view' }"
+          class="link"
+          to="/todo-list-view"
+          >Todo List</RouterLink
+        >
+        <RouterLink
+          :class="{ active: currentRoute === '/user-list-view' }"
+          class="link"
+          to="/user-list-view"
+          >User List</RouterLink
+        >
       </TransitionGroup>
     </div>
     <RouterView />
@@ -29,7 +55,6 @@ console.log(currentRoute)
 </template>
 
 <style>
-
 .list-move,
 .list-enter-active,
 .list-leave-active {
@@ -48,12 +73,12 @@ console.log(currentRoute)
 
 .list-container {
   border-style: solid;
-  padding: 10px ;
+  padding: 10px;
   display: flex;
-  margin-left:auto;
-  margin-right:auto;
+  margin-left: auto;
+  margin-right: auto;
   margin-top: 20px;
-  width: fit-content; 
+  width: fit-content;
   height: auto;
   justify-content: center;
   border-radius: 0.5rem;
@@ -61,11 +86,11 @@ console.log(currentRoute)
   z-index: 10;
 }
 
-.links-wrapper{
+.links-wrapper {
   display: flex;
-  justify-content:center;
-  justify-items:center;
-  gap:10px;
+  justify-content: center;
+  justify-items: center;
+  gap: 10px;
 }
 
 .link {
@@ -77,13 +102,11 @@ console.log(currentRoute)
   margin: auto;
 }
 
-.active{
-  background-color: green;;
+.active {
+  background-color: green;
   /* padding:10px; */
   border-style: solid;
   border-width: 1px;
   border-radius: 1rem;
-  
 }
-
 </style>
